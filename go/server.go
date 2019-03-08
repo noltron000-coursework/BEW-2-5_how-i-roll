506d674adcc758f13f6a07dcfeeb9578272a7617
Nolan Kovacik <noltron000@gmail.com>
2019-03-08 01:10:01 -0800

    😇 adding docstring

--- go/server.go
+++ go/server.go
@@ -1,5 +1,20 @@
 package main
 
+/* FILE DOCSTRINGS
+
+Major help came from these sources:
+  - Originally was going to use webassembly (*1)
+  - A good portion was learned from youtube (*2)
+  - Echo was a major resource in this go project
+  - Pieces were gathered from my classmates (*3)
+  - And of course Dani, Exercisms, and Go devs!!
+
+*1. https://tutorialedge.net/golang/go-webassembly-tutorial/
+*2. https://www.youtube.com/watch?v=_pww3NJuWnk
+*3. https://github.com/fchikwekwe/FaithBot
+
+END OF DOCSTRINGS */
+
 import (
 	"fmt"
 	"net/http"
